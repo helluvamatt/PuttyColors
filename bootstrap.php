b60cac54e27d9024d58785b9511a847625142a58
Matt Schneeberger <helluvamatt@gmail.com>
2017-01-08 14:06:25 -0600

    Go full circle: Use PHP/Slim under Apache/PHP7

---
+++ bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$config = [
+	'settings' => [
+		'displayErrorDetails' => true
+	]
+];
+$c = new \Slim\Container($config);
+$app = new \Slim\App($c);
+
+// View registration
+$container = $app->getContainer();
+$container['view'] = function($container) {
+	return new \Slim\Views\PhpRenderer(__DIR__ . '/templates');
+};
+
+// Routes
+$app->get("/[{id}]", function (Request $request, Response $response, $args) {
+	return $this->view->render($response, 'index.phtml', ['baseUrl' => $request->getUri()->getBasePath()]);
+})->setName('/');
+
+$app->run();
